8fa500c40a47eb77da9b6fac63ee15586f668f2f
mladenovic13 <nikolanik999@gmail.com>
2022-03-10 23:51:39 +0100

    New Header, Footer & Team component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,35 +1,150 @@
-import React from 'react';
-import Button from '../Button';
+import React, { useState } from 'react';
+import { Transition } from '@headlessui/react';
 import { StaticImage } from 'gatsby-plugin-image';
-import { Link } from 'gatsby';
+import Button from '../Button';
 
-const Header = () => (
-  <header className=" sticky top-0 bg-white shadow">
-    <div className="text-lg container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
-      <Link to="/">
-        <StaticImage className=" w-40 h-10" src="../../images/Logo.png" />
-      </Link>
-      <div className="flex mt-4 sm:mt-0">
-        <Link className="px-4" href="/about">
-          About Us
-        </Link>
-        <Link className="px-4" href="/solutions">
-          Solutions
-        </Link>
-        <Link className="px-4" href="/contact">
-          Contact Us
-        </Link>
-        <Link className="px-4" href="pricing">
-          Pricing
-        </Link>
-      </div>
-      <div className="hidden md:block">
-        <a href="https://floteq.sitelantern.com/console/">
-          <Button className=" text-base">Client Login</Button>
-        </a>
-      </div>
+function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <nav className="bg-white shadow w-full">
+        <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex-shrink-0">
+              <StaticImage className="h-8 w-32" src="../../images/Logo.png" alt="Workflow" />
+            </div>
+            <div className="flex items-center">
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <a
+                    href="#"
+                    className=" hover:text-primary-darker  text-primary px-3 py-2 rounded-md text-lg font-medium"
+                  >
+                    Solutions
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-primary hover:text-primary-darker px-3 py-2 rounded-md text-lg font-medium"
+                  >
+                    Pricing
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-primary  hover:text-primary-darker px-3 py-2 rounded-md text-lg font-medium"
+                  >
+                    About Us
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-primary  hover:text-primary-darker px-3 py-2 rounded-md text-lg font-medium"
+                  >
+                    Contact
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div>
+              <Button className="hidden md:block" size="default">
+                Client Login
+              </Button>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-primary-darker inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-primary-darker focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-200 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className=" text-primary-darker block px-3 py-2 rounded-md text-lg font-medium"
+                >
+                  Solutions
+                </a>
+
+                <a
+                  href="#"
+                  className="text-primary  hover:text-primary-darker block px-3 py-2 rounded-md text-lg font-medium"
+                >
+                  Pricing
+                </a>
+
+                <a
+                  href="#"
+                  className="text-primary  hover:text-primary-darker block px-3 py-2 rounded-md text-lg font-medium"
+                >
+                  About Us
+                </a>
+
+                <a
+                  href="#"
+                  className="text-primary  hover:text-primary-darker block px-3 py-2 rounded-md text-lg font-medium"
+                >
+                  Contact
+                </a>
+                <Button size="default">Client Login</Button>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
     </div>
-  </header>
-);
+  );
+}
 
-export default Header;
+export default Nav;
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import Team from '../components/Team';
-import TeamCard from '../components/TeamCard';
 
 export default function test() {
-  return (
-    <div>
-      <Team />
-    </div>
-  );
+  return <div></div>;
 }
