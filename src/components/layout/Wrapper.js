b5d634e3c2813bcd6995a3ea06b09b00eb4f2fba
mladenovic13 <nikolanik999@gmail.com>
2022-03-23 14:35:31 +0100

    Card Gradient

--- src/components/layout/Wrapper.js
+++ src/components/layout/Wrapper.js
@@ -2,7 +2,13 @@ import React, { Children } from 'react';
 
 const Wrapper = ({ isDark, children }) => {
   return (
-    <div className={`${isDark ? 'bg-zinc-100' : ''} m-4 p-8 lg:m-12 lg:p-16 rounded-md shadow-lg`}>
+    <div
+      style={{
+        backgroundImage:
+          'linear-gradient(135deg, rgba(255,255,255,0.00) 0%, rgba(239,239,239,0.33) 48%, rgba(193,193,193,0.35) 77%, #D8D8D8 100%)',
+      }}
+      className="m-4 p-8 lg:m-12 lg:p-16 rounded-md shadow-lg"
+    >
       {children}
     </div>
   );
